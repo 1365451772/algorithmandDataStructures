60da333b25636176e5dd0b1689f03b59b08ca5f0
sunpeng <1365451772@qq.com>
2020-12-17 13:25:36 +0800

    提交

--- dataStructures/src/com/atguigu/linkedList/Josephu.java
+++ dataStructures/src/com/atguigu/linkedList/Josephu.java
@@ -11,40 +11,41 @@ public class Josephu {
   public static void main(String[] args) {
     // 测试一把看看构建环形链表，和遍历是否ok
     CircleSingleLinkedList circleSingleLinkedList = new CircleSingleLinkedList();
-    circleSingleLinkedList.addBoy(125);// 加入5个小孩节点
-    circleSingleLinkedList.showBoy();
+//    circleSingleLinkedList.addBoy(125);// 加入5个小孩节点
+//    circleSingleLinkedList.showBoy();
 
     //测试一把小孩出圈是否正确
-//    circleSingleLinkedList.countBoy(10, 20, 125); // 2->4->1->5->3
-//    //String str = "7*2*2le-5+1-5+3-3";
+    circleSingleLinkedList.countBoy(10, 20, 125); // 2->4->1->5->3
+    //String str = "7*2*2le-5+1-5+3-3";
   }
 
 }
 
 
-
 //创建一个环形的单向链表
 class CircleSingleLinkedList {
+
   //创建一个first节点
   private Boy first = null;
+
   //添加Boy节点，构建成一个环形的链表
-  public void addBoy(int nums){
+  public void addBoy(int nums) {
     //nums 做一个数据校验
-    if (nums < 2 ){
+    if (nums < 2) {
       System.out.println("nums的值不正确");
       return;
     }
     Boy curBoy = null;//辅助指针，帮助构建环形列表
     //使用for循环来创建我们的环形列表
-    for (int i = 1;i<=nums;i++){
-        //根据编号，创建节点
+    for (int i = 1; i <= nums; i++) {
+      //根据编号，创建节点
       Boy boy = new Boy(i);
       //如果是第一个节点
-      if (i == 1){
+      if (i == 1) {
         first = boy;
         first.setNext(first);
         curBoy = first;
-      }else {
+      } else {
         boy.setNext(first);
         curBoy.setNext(boy);
         curBoy = boy;
@@ -54,21 +55,72 @@ class CircleSingleLinkedList {
   }
 
   //遍历循环单链表
-  public void showBoy(){
+  public void showBoy() {
     //判断链表是否为空
-    if (first == null){
+    if (first == null) {
       System.out.println("链表为空");
       return;
     }
     //first不动，创建辅助指针遍历
     Boy curBoy = first;
-    while (true){
-      System.out.printf("节点的编号%d\n",curBoy.getNo());
-      if (curBoy.getNext() == first){//遍历完毕
+    while (true) {
+      System.out.printf("节点的编号%d\n", curBoy.getNo());
+      if (curBoy.getNext() == first) {//遍历完毕
         break;
       }
       curBoy = curBoy.getNext();//指针后移
     }
+  }
+
+  // 根据用户的输入，计算出节点出圈的顺序
+
+  /**
+   * @param startNo 表示从第几个节点开始数数
+   * @param countNum 表示数几下
+   * @param nums 表示最初有多少节点在圈中
+   */
+  public void countBoy(int startNo, int countNum, int nums) {
+    this.addBoy(nums);
+    // 先对数据进行校验
+    if (first == null || startNo < 1 || startNo > nums) {
+      System.out.println("参数输入有误， 请重新输入");
+      return;
+    }
+
+    // 创建要给辅助指针,帮助完成节点出圈
+    Boy helper = first;
+    // 需求创建一个辅助指针(变量) helper , 事先应该指向环形链表的最后这个节点
+    while (true) {
+      if (helper.getNext() == first) { // 说明helper指向最后节点
+        break;
+      }
+      helper = helper.getNext();
+    }
+    //节点报数前，先让 first 和  helper 移动 k - 1次
+    for (int j = 0; j < startNo - 1; j++) {
+      first = first.getNext();
+      helper = helper.getNext();
+    }
+    //当节点报数时，让first 和 helper 指针同时 的移动  m  - 1 次, 然后出圈
+    //这里是一个循环操作，知道圈中只有一个节点
+    while (true) {
+      if (helper == first) { //说明圈中只有一个节点
+        break;
+      }
+      //让 first 和 helper 指针同时 的移动 countNum - 1
+      for (int j = 0; j < countNum - 1; j++) {
+        first = first.getNext();
+        helper = helper.getNext();
+      }
+      //这时first指向的节点，就是要出圈的节点
+      System.out.printf("节点%d出圈\n", first.getNo());
+      //这时将first指节点出圈
+      first = first.getNext();
+      helper.setNext(first); //
+
+    }
+    System.out.printf("最后留在圈中的节点编号%d \n", first.getNo());
+
   }
 
 }
