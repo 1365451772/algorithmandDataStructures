c3fdbebab1845bd1d665b2d2d45698a45c4528d9
sunpeng <1365451772@qq.com>
2020-12-18 16:49:42 +0800

    递归

--- dataStructures/src/com/atguigu/stack/ReversePolishMultiCalc.java
+++ dataStructures/src/com/atguigu/stack/ReversePolishMultiCalc.java
@@ -17,7 +17,7 @@ public class ReversePolishMultiCalc {
   static final String LEFT = "(";
   static final String RIGHT = ")";
   static final String ADD = "+";
-  static final String MINUS= "-";
+  static final String MINUS = "-";
   static final String TIMES = "*";
   static final String DIVISION = "/";
 
@@ -41,42 +41,34 @@ public class ReversePolishMultiCalc {
 
   /**
    * 去除所有空白符
-   * @param s
-   * @return
    */
-  public static String replaceAllBlank(String s ){
+  public static String replaceAllBlank(String s) {
     // \\s+ 匹配任何空白字符，包括空格、制表符、换页符等等, 等价于[ \f\n\r\t\v]
-    return s.replaceAll("\\s+","");
+    return s.replaceAll("\\s+", "");
   }
 
   /**
    * 判断是不是数字 int double long float
-   * @param s
-   * @return
    */
-  public static boolean isNumber(String s){
+  public static boolean isNumber(String s) {
 
     return pattern.matcher(s).matches();
   }
 
   /**
    * 判断是不是运算符
-   * @param s
-   * @return
    */
-  public static boolean isSymbol(String s){
+  public static boolean isSymbol(String s) {
     return s.matches(SYMBOL);
   }
 
   /**
    * 匹配运算等级
-   * @param s
-   * @return
    */
-  public static int calcLevel(String s){
-    if("+".equals(s) || "-".equals(s)){
+  public static int calcLevel(String s) {
+    if ("+".equals(s) || "-".equals(s)) {
       return LEVEL_01;
-    } else if("*".equals(s) || "/".equals(s)){
+    } else if ("*".equals(s) || "/".equals(s)) {
       return LEVEL_02;
     }
     return LEVEL_HIGH;
@@ -84,12 +76,14 @@ public class ReversePolishMultiCalc {
 
   /**
    * 匹配
-   * @param s
-   * @throws Exception
    */
-  public static List<String> doMatch (String s) throws Exception{
-    if(s == null || "".equals(s.trim())){ throw new RuntimeException("data is empty");}
-    if(!isNumber(s.charAt(0)+"")) {throw new RuntimeException("data illeagle,start not with a number");}
+  public static List<String> doMatch(String s) throws Exception {
+    if (s == null || "".equals(s.trim())) {
+      throw new RuntimeException("data is empty");
+    }
+    if (!isNumber(s.charAt(0) + "")) {
+      throw new RuntimeException("data illeagle,start not with a number");
+    }
 
     s = replaceAllBlank(s);
 
@@ -97,35 +91,35 @@ public class ReversePolishMultiCalc {
     int start = 0;
 
     for (int i = 0; i < s.length(); i++) {
-      if(isSymbol(s.charAt(i)+"")){
-        each = s.charAt(i)+"";
+      if (isSymbol(s.charAt(i) + "")) {
+        each = s.charAt(i) + "";
         //栈为空，(操作符，或者 操作符优先级大于栈顶优先级 && 操作符优先级不是( )的优先级 及是 ) 不能直接入栈
-        if(stack.isEmpty() || LEFT.equals(each)
-            || ((calcLevel(each) > calcLevel(stack.peek())) && calcLevel(each) < LEVEL_HIGH)){
+        if (stack.isEmpty() || LEFT.equals(each)
+            || ((calcLevel(each) > calcLevel(stack.peek())) && calcLevel(each) < LEVEL_HIGH)) {
           stack.push(each);
-        }else if( !stack.isEmpty() && calcLevel(each) <= calcLevel(stack.peek())){
+        } else if (!stack.isEmpty() && calcLevel(each) <= calcLevel(stack.peek())) {
           //栈非空，操作符优先级小于等于栈顶优先级时出栈入列，直到栈为空，或者遇到了(，最后操作符入栈
-          while (!stack.isEmpty() && calcLevel(each) <= calcLevel(stack.peek()) ){
-            if(calcLevel(stack.peek()) == LEVEL_HIGH){
+          while (!stack.isEmpty() && calcLevel(each) <= calcLevel(stack.peek())) {
+            if (calcLevel(stack.peek()) == LEVEL_HIGH) {
               break;
             }
             data.add(stack.pop());
           }
           stack.push(each);
-        }else if(RIGHT.equals(each)){
+        } else if (RIGHT.equals(each)) {
           // ) 操作符，依次出栈入列直到空栈或者遇到了第一个)操作符，此时)出栈
-          while (!stack.isEmpty() && LEVEL_HIGH >= calcLevel(stack.peek())){
-            if(LEVEL_HIGH == calcLevel(stack.peek())){
+          while (!stack.isEmpty() && LEVEL_HIGH >= calcLevel(stack.peek())) {
+            if (LEVEL_HIGH == calcLevel(stack.peek())) {
               stack.pop();
               break;
             }
             data.add(stack.pop());
           }
         }
-        start = i ;    //前一个运算符的位置
-      }else if( i == s.length()-1 || isSymbol(s.charAt(i+1)+"") ){
-        each = start == 0 ? s.substring(start,i+1) : s.substring(start+1,i+1);
-        if(isNumber(each)) {
+        start = i;    //前一个运算符的位置
+      } else if (i == s.length() - 1 || isSymbol(s.charAt(i + 1) + "")) {
+        each = start == 0 ? s.substring(start, i + 1) : s.substring(start + 1, i + 1);
+        if (isNumber(each)) {
           data.add(each);
           continue;
         }
@@ -142,15 +136,13 @@ public class ReversePolishMultiCalc {
 
   /**
    * 算出结果
-   * @param list
-   * @return
    */
-  public static Double doCalc(List<String> list){
+  public static Double doCalc(List<String> list) {
     Double d = 0d;
-    if(list == null || list.isEmpty()){
+    if (list == null || list.isEmpty()) {
       return null;
     }
-    if (list.size() == 1){
+    if (list.size() == 1) {
       System.out.println(list);
       d = Double.valueOf(list.get(0));
       return d;
@@ -158,12 +150,12 @@ public class ReversePolishMultiCalc {
     ArrayList<String> list1 = new ArrayList<>();
     for (int i = 0; i < list.size(); i++) {
       list1.add(list.get(i));
-      if(isSymbol(list.get(i))){
+      if (isSymbol(list.get(i))) {
         Double d1 = doTheMath(list.get(i - 2), list.get(i - 1), list.get(i));
         list1.remove(i);
-        list1.remove(i-1);
-        list1.set(i-2,d1+"");
-        list1.addAll(list.subList(i+1,list.size()));
+        list1.remove(i - 1);
+        list1.set(i - 2, d1 + "");
+        list1.addAll(list.subList(i + 1, list.size()));
         break;
       }
     }
@@ -173,19 +165,24 @@ public class ReversePolishMultiCalc {
 
   /**
    * 运算
-   * @param s1
-   * @param s2
-   * @param symbol
-   * @return
    */
-  public static Double doTheMath(String s1,String s2,String symbol){
-    Double result ;
-    switch (symbol){
-      case ADD : result = Double.valueOf(s1) + Double.valueOf(s2); break;
-      case MINUS : result = Double.valueOf(s1) - Double.valueOf(s2); break;
-      case TIMES : result = Double.valueOf(s1) * Double.valueOf(s2); break;
-      case DIVISION : result = Double.valueOf(s1) / Double.valueOf(s2); break;
-      default : result = null;
+  public static Double doTheMath(String s1, String s2, String symbol) {
+    Double result;
+    switch (symbol) {
+      case ADD:
+        result = Double.valueOf(s1) + Double.valueOf(s2);
+        break;
+      case MINUS:
+        result = Double.valueOf(s1) - Double.valueOf(s2);
+        break;
+      case TIMES:
+        result = Double.valueOf(s1) * Double.valueOf(s2);
+        break;
+      case DIVISION:
+        result = Double.valueOf(s1) / Double.valueOf(s2);
+        break;
+      default:
+        result = null;
     }
     return result;
 
