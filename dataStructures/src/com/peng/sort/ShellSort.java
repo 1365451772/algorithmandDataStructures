2260d5258ab1d0f56d2999af2672b3fd90016e32
sunpeng <1365451772@qq.com>
2021-01-05 16:57:59 +0800

    排序

--- dataStructures/src/com/peng/sort/ShellSort.java
+++ dataStructures/src/com/peng/sort/ShellSort.java
@@ -9,10 +9,13 @@ public class ShellSort {
 
   public static void main(String[] args) {
     int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+    shellSort(arr);
   }
 
   public static void shellSort(int[] arr) {
+    int n = 0;
     for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+      System.out.println(++n);
       //从第gap个元素，逐个对其所在的组进行直接
       for (int i = gap; i < arr.length; i++) {
         int j = i;
